82989ce482e3cc543cafbd033167740c70adca57
Matias <matiasmarin45@gmail.com>
2022-03-02 18:23:52 -0300

    Current user expectation changed

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ describe('Current User Test', () => {
       .get('/api/users/currentuser')
       .set('Cookie', cookie)
       .send()
-      .expect(400);
+      .expect(200);
 
     expect(response.body.currentUser.email).toEqual('test@test.com');
   });
