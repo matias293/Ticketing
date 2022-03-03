37710b674fcd4959da591e43b3ff2e3acc4d2a11
Matias <matiasmarin45@gmail.com>
2022-03-03 14:36:50 -0300

    Change

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting the app');
+  console.log('Starting the app.');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
